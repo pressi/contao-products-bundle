925986181798ec89dc847dd8c8d8c06f18ad12b6
Stephan Preßl <mail@stephanpressl.at>
2019-02-05 16:31:35 +0100

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,16 @@
+<?php
+/*******************************************************************
+ *
+ * (c) 2019 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ *
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *
+ *******************************************************************/
+
+$GLOBALS['BE_MOD']['content']['prestepProducts'] = array
+(
+    'tables' => array('tl_prestep_products_archive', 'tl_prestep_product', 'tl_prestep_product_article', 'tl_content')
+);
